5054edd1ac059ae84b5c1395642197924dda52b0
GoGoGadgetGitHub <christobothma258@gmail.com>
2024-11-30 05:34:49 +0200

    working on authentication

---
+++ src/js/login.js
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://jpxdwuzsxkcerplprlwv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweGR3dXpzeGtjZXJwbHBybHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4MDQ4MTksImV4cCI6MjA0ODM4MDgxOX0.lFBWgHBURcsJ-niq0E5t4arJQdrDxQA_o2-uhZ6Q9r0' 
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function signUpNewUser() {
+    const { data, error } = await supabase.auth.signUp({
+      email: 'christobothma258@gmail.com',
+      password: 'example-password',
+    })
+ }
+ 
+ signUpNewUser()
+
+  
